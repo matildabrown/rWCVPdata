cfd06ba233cea07fd22449a8458d1708d8cd7d06
barnabywalker <barnaby.walker08@gmail.com>
2023-01-11 14:53:04 +0000

    added script to generate WCVP data

---
+++ data-raw/wcvp.R
@@ -0,0 +1,68 @@
+## code to download the latest WCVP name and distribution tables and record version
+## re-run this to update the WCVP data when there is a new release
+## REMEMBER TO UPDATE THE DOCS IF THE DATA STRUCTURE HAS CHANGED
+
+library(httr)
+library(rvest)
+library(stringr)
+library(readr)
+library(readxl)
+library(lubridate)
+library(glue)
+
+# download save wcvp data ----
+base_url <- "http://sftp.kew.org/pub/data-repositories/WCVP/"
+zip_url <- paste0(base_url, "wcvp.zip")
+
+# download to temporary place and extract
+temp <- tempfile()
+download.file(zip_url, temp)
+unzip(temp, exdir="wcvp-files")
+
+# load and save the names file
+wcvp_names <- read_delim("wcvp_names.csv", delim="|", quote="")
+usethis::use_data(wcvp_names, compress="xz", overwrite=TRUE)
+
+# load and save the distributions file
+wcvp_distributions <- read_delim("wcvp_distribution.csv", delim="|", quote="")
+usethis::use_data(wcvp_distributions, compress="xz", overwrite=TRUE)
+
+# extract metadata ----
+# get info from README spreadsheet
+version <- read_xlsx("README_WCVP.xlsx", range="A7", col_names="version")$version
+version <- str_extract(version, "\\d+")
+
+citation <- read_xlsx("README_WCVP.xlsx", range="A4", col_names="cite")$cite
+cite_date <- str_extract(citation, "(?<=accessed )\\d+ [A-Z][a-z]+ \\d{4}")
+cite_date <- as_date(cite_date, format="%d %m %Y")
+
+table_info <- read_xlsx("README_WCVP.xlsx", range="A11", col_names="info")$info
+table_rows <- str_extract(table_info, "[\\d\\,]+(?= rows)")
+table_cols <- str_extract(table_info, "[\\d\\,]+(?= columns)")
+
+# Parse upload date from SFPT server site
+r <- GET(base_url)
+page <- httr::content(r)
+
+table_node <- html_node(page, "pre")
+table_text <- html_text(table_node)
+
+table_lines <- str_split(table_text, "\n")[[1]]
+
+wcvp_line <- table_lines[str_detect(table_lines, "wcvp.zip")]
+upload_date <- str_extract(wcvp_line, "\\d{4}-\\d{2}-\\d{2}")
+
+# save to internal data file
+metadata <- list(
+  version=as.numeric(version),
+  version_date=glue("{month(cite_date, label=T, abbr=T)} {year(cite_date)}"),
+  name_rows=as.numeric(table_rows),
+  name_col=as.numeric(table_cols),
+  citation=citation
+)
+
+usethis::use_data(metadata, internal=TRUE, overwrite=TRUE)
+
+# clean up directory ----
+unlink(temp)
+unlink("wcvp-files", recursive=TRUE)
